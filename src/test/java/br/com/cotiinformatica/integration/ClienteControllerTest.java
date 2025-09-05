7d7d042e0558449ebe2d47ca6c0396fbdb7cf7d5
cotiaws <cotiaws5@gmail.com>
2025-09-05 20:49:04 -0300

    feat: estrutura inicial do projeto com testes unitários e de integração falhando.

---
+++ src/test/java/br/com/cotiinformatica/integration/ClienteControllerTest.java
@@ -0,0 +1,45 @@
+package br.com.cotiinformatica.integration;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClienteControllerTest {
+
+	@BeforeEach
+	public void setUp() {
+		// TODO Implementar
+	}
+
+	@Test
+	@DisplayName("POST /api/v1/clientes - Deve retornar 201 ao cadastrar.")
+	public void postClientesReturnsCreated() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("PATCH /api/v1/clientes - Deve retornar 200 ao atualizar.")
+	public void patchClientesReturnsOk() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("DELETE /api/v1/clientes - Deve retornar 200 ao excluir.")
+	public void deleteClientesReturnsOk() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("GET /api/v1/clientes - Deve retornar 200 ao consultar todos.")
+	public void getAllClientesReturnsOk() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("GET /api/v1/clientes - Deve retornar 200 ao consultar 1 cliente por ID.")
+	public void getByIdClientesReturnsOk() {
+		fail("Não implementado"); // TODO Implementar
+	}
+}
---
+++ src/test/java/br/com/cotiinformatica/unit/ClienteServiceTest.java
@@ -0,0 +1,45 @@
+package br.com.cotiinformatica.unit;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClienteServiceTest {
+
+	@BeforeEach
+	public void setUp() {
+		// TODO Implementar
+	}
+
+	@Test
+	@DisplayName("Deve criar um cliente com sucesso.")
+	public void deveCriarCliente() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("Deve atualizar um cliente com sucesso.")
+	public void deveAtualizarCliente() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("Deve inativar um cliente com sucesso.")
+	public void deveInativarCliente() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("Deve consultar clientes de forma paginada com sucesso.")
+	public void deveConsultarClientes() {
+		fail("Não implementado"); // TODO Implementar
+	}
+
+	@Test
+	@DisplayName("Deve obter 1 cliente pelo ID com sucesso.")
+	public void deveObterCliente() {
+		fail("Não implementado"); // TODO Implementar
+	}
+}
